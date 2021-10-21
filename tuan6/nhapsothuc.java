7485597ffc31062381b857cf474d8a7060b3f1cc
Djum542 <djum54022@gmail.com>
2021-10-21 15:59:19 +0700

    đẩy

--- tuan6/nhapsothuc.java
+++ tuan6/nhapsothuc.java
@@ -1,6 +1,11 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.atomic.DoubleAccumulator;
+
 public class Main {
     public static void main(String[] args) {
-        
+        ArrayList(Double) a = new ArrayList<>();
+        List 
     }
-    
+
 }
