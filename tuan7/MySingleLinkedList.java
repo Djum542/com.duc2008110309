1293f45d208e2fe62441d31a1f897fef284c8763
Djum542 <djum54022@gmail.com>
2021-10-23 08:10:53 +0700

    đẩy tuần

---
+++ tuan7/MySingleLinkedList.java
@@ -0,0 +1,43 @@
+public class MySingleLinkedList {
+    Node head, tail;
+
+    public MySingleLinkedList() {
+        head = tail = null;
+    }
+
+    public addNode(int data){
+        // tao mơi nut
+        Node newNode = new Node(data);
+        //kiem tra danh sach
+        if (head = null) {
+            //neu danh sach rong
+            head = newNode;
+            tail = newNode;
+            
+        }
+        else{
+            //newNode se duoc lien ket
+            // thanh phan lien ket
+            tail.next = newNode;
+            // newNode se tro thanh nut cuoi
+            tail = newNode;
+        }
+    }
+
+    public void frint{
+        // tạo một nút (curent) giữ nút (head)
+        Node current = head;
+        if(head = null){
+            System.out.println("Danh sach rong");
+            return;
+
+        }
+        System.out.println("Xuat ra cac nut tren lien ket");
+        while () {current != null){
+            System.out.println(current.info + " ");
+            current = current.next;
+            }
+            System.out.println();
+        }
+    }
+}
