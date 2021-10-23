0f3802153690f1723cbe116930ea2402da8441c6
Djum542 <djum54022@gmail.com>
2021-10-23 08:10:26 +0700

    đẩy tuần 7

---
+++ tuan7/Node.java
@@ -0,0 +1,19 @@
+public class Node {
+    int info;// vung du lieu
+    Node next;// vung lieu
+
+    public Node() {// ham tao rong
+    }
+
+    // ham tao co mot tham so
+    public Node(int data){
+         info = data;
+
+     }
+
+    public Node(int x, n){
+         info = x;
+         next = n;
+     }
+     
+}
---
+++ tuan7/NodetestDrive.java
@@ -0,0 +1,9 @@
+import java.lang.ProcessHandle.Info;
+
+import org.w3c.dom.Node;
+
+public class NodetestDrive {
+    public static void main(String[] args) {
+
+    }
+}
