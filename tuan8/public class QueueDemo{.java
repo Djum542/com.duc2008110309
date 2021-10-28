044accf6ef80c85eb473019fca99fde731805e45
Djum542 <djum54022@gmail.com>
2021-10-28 09:10:51 +0700

    đẩy tuần

---
+++ tuan8/public class QueueDemo{.java
@@ -0,0 +1,20 @@
+public class QueueDemo{
+    public static void main(String[] args) {
+        queue<Integer> queue;
+        queue = new LinkedList<>();
+        // dùng LinkedList đẻ cài đặt queue
+            // Thêm phần tử vào hàng chờ ở cuối hàng
+            // Lấy 1 phần tử ở đầu mảng
+            // Thăm 1 phần tử ở đầu mang
+            queue.add(2;
+            queue.add(15);
+            queue.add(44);
+            System.out.println(queue);
+            // Thăm phần tử 
+            int phantuDuoc = queue.peek();
+            System.out.println("phần tư cần thăm" + phantuDuoc);
+            System.out.println(" phần tử còn lại");
+            System.out .println(queue);
+
+    }
+}
