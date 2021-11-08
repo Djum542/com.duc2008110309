e40c258e08586a85399538ae4185d695ced3fd4c
Djum542 <djum54022@gmail.com>
2021-11-08 12:03:17 +0700

    day

--- kiemtraGKlan2/BookNodetestDriver.java
+++ kiemtraGKlan2/BookNodetestDriver.java
@@ -4,17 +4,17 @@ import java.util.LinkedList;
 
 public class BookNodetestDriver {
     public static void main(String[] args) {
-        public static LinkedList bienNhap = new LinkedList<>();
+        public static LinkedList book = new LinkedList<>();
             System.out.println("Chưa có sach: ");
             System.out.print("Hãy nhập ID sach: ");
-            int id = bienNhap.nextInt();
+            int id = book.nextInt();
             
             System.out.print("Hãy nhập don gia: ");
-            float DG = bienNhap.nextFloat();
+            float DG = book.nextFloat();
             
             System.out.print("Hãy nhập so luong: ");
             bienNhap.nextLine();
-            String SL = bienNhap.nextLine();
+            String SL = book.nextLine();
             
             BookNode book = new BookNode(id, DG, SL);
             BookNode head = book;
--- kiemtraGKlan2/booktestDriver.java
+++ kiemtraGKlan2/booktestDriver.java
@@ -5,15 +5,13 @@ import java.util.ArrayList;
 public class booktestDriver {
     public static void main(String[] args) {
         public static ArrayList book = new ArrayList<>();
-        
-            System.out.println("Chưa co sach moi them");
-            System.out.print("Hãy nhập ID sach");
-            int id = book.nextInt();
             book.add("1235894", "12.000", "5");
             book.add("1258947", "24.000", "7");
             book.add("1578254", "30.000", "2");
-
-
+            System.out.println("Chưa co sach moi them");
+            System.out.print("Hãy nhập ID sach");
+            int id = book.nextInt();
+            
             System.out.print("Hãy nhập don gia: ");
             float DG = book.nextFloat();
             
