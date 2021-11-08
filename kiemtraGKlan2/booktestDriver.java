aa74b528e71c5a08c990c29ea18520cddd2eff27
Djum542 <djum54022@gmail.com>
2021-11-08 11:59:07 +0700

    day

--- kiemtraGKlan2/booktestDriver.java
+++ kiemtraGKlan2/booktestDriver.java
@@ -4,17 +4,22 @@ import java.util.ArrayList;
 
 public class booktestDriver {
     public static void main(String[] args) {
-        public static ArrayList bienNhap = new ArrayList<>();
+        public static ArrayList book = new ArrayList<>();
+        
             System.out.println("Chưa co sach moi them");
             System.out.print("Hãy nhập ID sach");
-            int id = bienNhap.nextInt();
-            
+            int id = book.nextInt();
+            book.add("1235894", "12.000", "5");
+            book.add("1258947", "24.000", "7");
+            book.add("1578254", "30.000", "2");
+
+
             System.out.print("Hãy nhập don gia: ");
-            float DG = bienNhap.nextFloat();
+            float DG = book.nextFloat();
             
             System.out.print("Hãy nhập so luong: ");
             bienNhap.nextLine();
-            String SL = bienNhap.nextLine();
+            String SL = book.nextLine();
             
            
     }
