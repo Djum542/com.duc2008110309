4be4d5f08809e295960076fc3a99ca134e47d724
Djum542 <djum54022@gmail.com>
2021-11-08 07:00:20 +0700

    đẩy

---
+++ tuan9/timapplebangluachon.java
@@ -0,0 +1,13 @@
+import java.util.LinkedList;
+
+public class timapplebangluachon{
+    public static void main(String[] args) {
+        apple = new LinkedList<String>();
+        while (return) {
+            apple.add();
+        }
+        
+        selectionSort = new LinkedList<Boolean>();
+        System.out.println(selectionSort);
+    }
+}
