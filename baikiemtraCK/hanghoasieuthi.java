c30d44cfb5113248e40fb99d0a2b95673352258e
Djum542 <djum54022@gmail.com>
2021-11-19 10:21:55 +0700

    đẩy lần 1

--- baikiemtraCK/hanghoasieuthi.java
+++ baikiemtraCK/hanghoasieuthi.java
@@ -1,3 +1,63 @@
+package BAIKIEMTRACK;
+
+import java.text.NumberFormat;
+import java.util.Calendar;
+import java.util.Locale;
+import java.util.Scanner;
+
 public class hanghoa {
-    
+    int loai;
+    int id;
+    int TenSP;
+    float DG;
+    double SL;
+    date NgN;
+    Scanner nhap = new scanner(System.in);
+    hanghoa(){
+
+    }
+     hanghoa(int loai;int id;int TenSP;float DG;double SL;date NgN){
+        loai = loai;
+        id = id;
+        TenSP = TenSP;
+        DG = DG;
+        SL = SL;
+        NgN = Ng;
+
+    }
+    void nhaphanghoa(){
+        System.out.println("Nhập loại hàng");
+        loai = nhap.nextInt();
+        System.out.println("Nhập mã hàng");
+        id = nhap.nextInt();
+        System.out.println("Nhập tên hàng");
+        TenSP = nhap.nextInt();
+        System.out.println("Nhập dơn giá");
+        DG = nhap.nextFloat();
+        System.out.println("Nhập ngày nhập hàng");
+        id = nhap.nextInt();
+    }
+    public String toStrong(){
+        Locale localeid = new Locale("id", "ma");
+        NumberFormat id = id.getCurrencyInstance(localeid);
+        String id = NumberFormat.format(id);
+    }
+    public void NgN(int year, int month, int day){
+        Calendar NgN = Calendar.getInstance();
+        Calendar.set(year, month - 1, day);
+        this.NgN = Calendar.getTime();
+    }
+    void inhanghoa(){
+        System.out.println("loại"+loai);
+        System.out.println("mã"+id); 
+        System.out.println("tên hàng "+TenSP);
+        System.out.println("đơn giá"+DG);
+        System.out.println("ngày nhap hàng"+NgN);
+
+
+
+
+    }
+   
+
 }
