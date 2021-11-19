1d56631187557eb5397cb25559c42f7bbf218082
Djum542 <djum54022@gmail.com>
2021-11-19 10:52:04 +0700

    cập nhật

--- baikiemtraCK/src/dshanghoa.java
+++ baikiemtraCK/src/dshanghoa.java
@@ -1,3 +1,21 @@
+package com.ductm2008110309.baikiemtraCK
+
+import org.w3c.dom.Node;
+
 public class dshanghoa {
-    node
+    Node head = null;
+    Node tail = null;
+    String nhap = new String();
+    int loai;
+    int id;
+    int TenSP;
+    float DG;
+    double SL;
+    date NgN;
+    public dshanghoa(){
+
+    }
+    public void (){
+        int curent;
+    }
 }
