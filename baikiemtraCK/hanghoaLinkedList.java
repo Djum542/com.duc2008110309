d5de167d9651da966b043131fec189cfdd4cb003
Djum542 <djum54022@gmail.com>
2021-11-30 13:58:31 +0700

    đẩy

--- baikiemtraCK/hanghoaLinkedList.java
+++ baikiemtraCK/hanghoaLinkedList.java
@@ -15,6 +15,30 @@ public class hanghoaLinkedList {
     double SL;
     date NgN;
     public hanghoaLinkedList(){
+        hanghoaLinkedList<String> Hanghoa = new LinkedList<>();
+        Hanghoa.add("Thực phẩm ", "THP358784", "thịt", "150.000", "150", "30/11/2021");
+        Hanghoa.add("Thực phẩm ", "THP676575", "Bơ ", "15.000", "300", "30/11/2021");
+        Hanghoa.add("Thực phẩm ", "THP629388", "sữa chua", "20.000", "1000", "30/11/2021");
+        Hanghoa.add("Thực phẩm ", "THP532837", "Xúc xích", "55.000", "1200", "30/11/2021");
+        Hanghoa.add("Thực phẩm ", "THP237382", "Lạp xưởng", "180.000", "500", "30/11/2021");
+        Hanghoa.add("Thực phẩm ", "THP236382", "Kem", "4.000", "325", "30/11/2021");
+        Hanghoa.add("Thực phẩm ", "THP364647", "Dầu thực vật", "62.000", "150", "30/11/2021");
+        Hanghoa.add("Thực phẩm ", "THP472829", "Mì tôm", "135.000", "500", "30/11/2021");
+        Hanghoa.add("Thực phẩm ", "THP382362", "Mực", "120.000", "650", "30/11/2021");
+        Hanghoa.add("Thực phẩm ", "THP469327", "Tôm", "90.000", "700", "30/11/2021");
+        Hanghoa.add("Thực phẩm ", "THP434938", "Gạo", "14.000", "1000", "30/11/2021");
+        Hanghoa.add("Thực phẩm ", "THP537498", "Ngũ cốc", "68.000", "727", "30/11/2021");
+        Hanghoa.add("Thực phẩm ", "THP456343", "Sữa", "240.000", "3540", "30/11/2021");
+        Hanghoa.add("Thực phẩm ", "THP343873", "Bia", "167.000", "4500", "30/11/2021");
+        Hanghoa.add("Thực phẩm ", "THP463745", "Nước ngọt", "448.000", "4300", "30/11/2021");
+        Hanghoa.add("Thực phẩm ", "THP564364", "Gia vị", "745.000", "4578", "30/11/2021");
+        Hanghoa.add("Gốm sứ", "GS585494", "Bình ", "88.000", "380", "25/07/2021");
+        Hanghoa.add("Gốm sứ ", "GS358784", "Hộp ", "125.000", "4678", "25/07/2021");
+        Hanghoa.add("Gốm sứ ", "GS568338", "Đĩa", "134.000", "457", "25/07/2021");
+        Hanghoa.add("Điện tử ", "DT563478", "tivi", "3.460.000", "487", "06/5/2021");
+        Hanghoa.add("Điện tử ", "DT647346", "Tủ lạnh", "6.760.000", "867", "06/5/2021");
+        Hanghoa.add("Điện tử ", "DT675438", "Lò vi ba", "4.050.000", "486", "06/5/2021");
+        Hanghoa.add("Điện tử ", "DT5462384", "Quạt ", "1.578.000", "459", "06/5/2021");
 
     }
     public boolean isEmtpy(hanghoa HH){
